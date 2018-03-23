8d0c322ca6bca2ca489d75f2ef4b7723d0bf8378
Ammar iqbal <ammariqbal127@gmail.com>
2018-03-24 04:41:40 +0500

    90set

---
+++ routes/index.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var router = express.Router();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Test');
+
+var db = mongoose.connection;
+// check connection
+
+db.once('open',function () {
+    console.log('Conected to database');
+
+});
+var Schema = mongoose.Schema;
+
+var TASKSchema = new Schema(
+    {
+        text: {type: String, required: true},
+        status: {type: Boolean, required:true}
+    } );
+
+var task = mongoose.model('TASK', TASKSchema);
+
+
+//check for db errors
+db.on('error',function (err) {
+    console.log(err);
+
+});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+    task.find()
+        .then(function(doc) {
+            if(doc) {
+
+                task.count({status:false},function (err,d) {
+                    if(err){
+                        console.log("Rooooooooooooo");
+                        console.log(err);
+                    }
+                    else {
+
+                        res.render('index', {items: doc ,coun:d });
+                    }
+
+                });
+
+            }
+            else
+                res.render('index');
+        });
+
+});
+
+// get all active tasks
+router.get('/active', function(req, res, next) {
+
+    task.find({status:false})
+        .then(function(doc) {
+            res.render('index', {items: doc});
+        });
+   // res.render('index');
+});
+// get all completed tasks
+router.get('/complete', function(req, res, next) {
+
+    task.find({status:true})
+        .then(function(doc) {
+            res.render('index', {items: doc});
+        });
+    //res.render('index');
+});
+
+// Save the task
+router.post('/save', function(req, res, next) {
+var a = new task();
+a.text = req.body.tas;
+a.status = false;
+
+a.save();
+
+res.redirect('/');
+});
+router.get('/update/:id', function(req, res, next) {
+    var id = req.params.id;
+
+    task.findById(id, function(err, doc) {
+
+        doc.status = !doc.status;
+
+        doc.save();
+    })
+    res.redirect('/');
+});
+
+router.get('/delete/:id', function(req, res, next) {
+    var id = req.params.id;
+
+    task.findById(id,function (err,doc) {
+       if(err){
+           console.log(err);
+           res.redirect('/');
+       }
+       else
+       {
+           console.log(doc);
+           doc.remove();
+           res.redirect('/');
+       }
+    });
+
+
+});
+
+// Delete all complted
+router.get('/completed', function(req, res, next) {
+
+   task.remove({status:true},function (err) {
+        console.log(err);
+    });
+   res.redirect('/');
+});
+module.exports = router;
